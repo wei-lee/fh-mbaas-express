b34b51135d963394c6829c9a5584a3e86446e27b
cianclarke <cian@cianclarke.com>
2013-08-01 15:56:10 -0400

    Allow sys to be initd with no main.js

--- test/fixtures/application.js
+++ test/fixtures/application.js
@@ -1,11 +1,12 @@
-var webapp = require('fh-webapp').init(require('main.js')); //TODO: Implicitly requires mainjs :-(
+var webapp = require('fh-webapp');
 var express = require('express');
+var mainjs = require('main.js');
 //$fh = require('fh-api'); // TODO: Write fh-api
 
 var app = express();
 //app.use(express.bodyParser()); // this causes issues. Why?
-app.use('/sys', webapp.sys);
+app.use('/sys', webapp.sys(mainjs));
 app.use('/mbass', webapp.mbaas);
-app.use('/cloud', webapp.cloud);
+app.use('/cloud', webapp.cloud(mainjs));
 
 module.exports = app.listen(3000);
