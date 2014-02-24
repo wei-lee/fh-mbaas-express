368d78eaa4b7c7eecf5b052b8426d804dfab694a
Martin Murphy <martin.murphy@feedhenry.com>
2014-02-24 14:38:05 +0000

    get deviceID from getConfig api and pass on to fh-forms

--- test/accept/test_mbaas_forms.js
+++ test/accept/test_mbaas_forms.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var request = require('request');
 var nock = require('nock');
 var fs = require('fs');
+var util = require('util');
 
 module.exports = {
   "setUp" : function(finish){
@@ -9,6 +10,51 @@ module.exports = {
   },
   "tearDown" : function(finish){
     finish();
+  },
+  "test_form_call_to_getconfig_with_no_deviceID" : function(finish){
+    request.get(process.env.FH_TEST_HOSTNAME + "/mbaas/forms/appid/config",
+      {
+        json:{
+        },
+        headers : {
+          'Content-Type' : 'application/json',
+        }
+      },
+      function(err, res, data){
+        console.log(" ***** CONFIG ******* return data ", err, data);
+        assert.ok(!err);
+        assert.ok(data);
+        assert.ok(data.status !== "ok", "should have returned error: not deviceid sent");
+        assert.notEqual(res.statusCode, 200, 'should have returned http error - status: ' + util.inspect(res.statusCode));
+        finish();
+      });
+  },
+  "test_form_call_to_getconfig" : function(finish){
+    var api = "/mbaas/forms/{{appid}}/config/{{deviceid}}";
+    var TEST_APPID = "appid";
+    var TEST_DEVICEID = "myspecialdevice";
+
+    var url = process.env.FH_TEST_HOSTNAME + api.replace("{{appid}}", TEST_APPID).replace("{{deviceid}}", TEST_DEVICEID);
+
+    request.get(url,
+      {
+        json:{
+        },
+        headers : {
+          'Content-Type' : 'application/json',
+        }
+      },
+      function(err, res, data){
+        console.log(" ***** CONFIG ******* return data ", err, data);
+        assert.ok(!err);
+        assert.ok(data);
+        assert.ok(data.status === "ok");
+        assert.equal(data.called, "getAppClientConfig", "called incorrect forms function: " + util.inspect(data));
+        assert.equal(data.params.deviceId, TEST_DEVICEID, "did not pass device ID to forms function: " + util.inspect(data));
+        assert.equal(data.params.appId, TEST_APPID, "did not pass app ID to forms function: " + util.inspect(data));
+
+        finish();
+      });
   },
   "test_form_call_to_getForms" : function(finish){
     request.get(process.env.FH_TEST_HOSTNAME + "/mbaas/forms/appid",
